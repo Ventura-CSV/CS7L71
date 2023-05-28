581bc3f8756ea3dcfcee07260202e5e5417babb7
Kyu Lee <kwooler@gmail.com>
2023-05-28 14:00:44 -0700

    L71

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-def main():
-    ##################################################
-    # Comlete your code here
-    ##################################################
 
-    pass
+def getSum(numbers):
+    """
+    ########################################
+    Code Your Program here
+    ########################################
+    """
+
+
+def main():
+    numbers = [10, 25, 15, 35, 50]
+    total = getSum(numbers)
+    print('The total withtou min and max', total)
 
 
 if __name__ == '__main__':
--- main_test.py
+++ main_test.py
@@ -5,40 +5,30 @@ import re
 
 
 def test_main_1():
-    captureOut = io.StringIO()
-    sys.stdout = captureOut
-    datastr = '10\n25\n15\n35\n50'
-    sys.stdin = io.StringIO(datastr)
+    # captureOut = io.StringIO()
+    # sys.stdout = captureOut
+    # # datastr = '10\n25\n15\n35\n50'
+    # # sys.stdin = io.StringIO(datastr)
 
-    main.main()
-    sys.stdout = sys.__stdout__
-    print('Captured ', captureOut.getvalue())
-    lines = captureOut.getvalue().split('\n')
-    print(lines)
+    # sys.stdout = sys.__stdout__
+    # print('Captured ', captureOut.getvalue())
+    # lines = captureOut.getvalue().split('\n')
+    # print(lines)
 
-    regex_string = r'[\w,\W]*75'
-    regex_string += r'[\w,\W]*'
-    print(regex_string)
-    res = re.search(regex_string, lines[0])
-    assert res != None
-    print(res.group())
+    # regex_string = r'[\w,\W]*75'
+    # regex_string += r'[\w,\W]*'
+    # print(regex_string)
+    # res = re.search(regex_string, lines[0])
+    # assert res != None
+    # print(res.group())
+    numbers = [10, 25, 15, 35, 50]
+    ret = main.getSum(numbers)
+    print(f'The return value is {ret}')
+    assert ret == 75
 
 
 def test_main_2():
-    captureOut = io.StringIO()
-    sys.stdout = captureOut
-    datastr = '1\n2\n3\n4\n5'
-    sys.stdin = io.StringIO(datastr)
-
-    main.main()
-    sys.stdout = sys.__stdout__
-    print('Captured ', captureOut.getvalue())
-    lines = captureOut.getvalue().split('\n')
-    print(lines)
-
-    regex_string = r'[\w,\W]*9'
-    regex_string += r'[\w,\W]*'
-    print(regex_string)
-    res = re.search(regex_string, lines[0])
-    assert res != None
-    print(res.group())
+    numbers = [3, 8, 2, 6, 1, 9, 4, 7]
+    ret = main.getSum(numbers)
+    print(f'The return value is {ret}')
+    assert ret == 30
